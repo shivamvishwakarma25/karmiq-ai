a88d02d26cfc2868306f6357c29fa06a68dad4df
shivamvishwakarma25 <90044464+shivamvishwakarma25@users.noreply.github.com>
2025-11-08 16:24:54 +0530

    Fix portfolio tech stack and links

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,22 +7,26 @@ const portfolioData = [
   {
     title: 'E-commerce AI Chatbot',
     description: 'Intelligent chatbot with RAG for product recommendations and customer support',
-    tech: ['LangChain', 'RAG', 'Groq API', 'Streamlit']
+    tech: ['LangChain', 'RAG', 'Groq API', 'Streamlit'],
+    link: 'https://e-commerce-faq-chatbot-gyyq8blsect2j7z75pwxdi.streamlit.app/'
   },
   {
     title: 'KarmiQ AI Assistant',
     description: 'Interactive AI assistant for customers with voice capabilities and information of KarmiQ AI',
-    tech: ['Vapi', 'OpenAI', 'CRM', 'IVR']
+    tech: ['Vapi', 'OpenAI', 'CRM', 'IVR'],
+    link: 'https://vapi.ai?demo=true&shareKey=18bd66d2-a09c-4c6c-941e-c78e2157b458&assistantId=d3ad9a30-3057-42b3-b8d0-20c659a8e7b6'
   },
   {
     title: 'Content Generation Platform',
     description: 'AI-powered blog and social media content generator with SEO optimization',
-    tech: ['LLM', 'HuggingFace', 'Python', 'NLP']
+    tech: ['Veo 3.1', 'FLOW', 'HeyGen', 'Nano Banana']
+    link:'https://drive.google.com/file/d/1xZ_kZkFOUX6UASrsrkixT-0RRrFLyatw/view?usp=sharing'
   },
   {
     title: 'Blogify - AI Blog Generator',
     description: 'An AI-powered blog writing assistant that generates engaging blog posts based on a title and keywords.',
-    tech: ['LLM', 'HuggingFace', 'LangChain', 'NLP']
+    tech: ['LLM', 'HuggingFace', 'LangChain', 'NLP'],
+    link: 'https://blogify-ds3e3ok4b5dkr3bybt28o8.streamlit.app/'
   },  
 
 ];
